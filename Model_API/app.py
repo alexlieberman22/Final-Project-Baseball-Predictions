983f064b71bb74b50cb2c3dc188d9d5e560872f4
Eduardo Perez <eduardoevperez@gmail.com>
2022-05-18 19:10:40 -0600

    Push starter flask, css and html to repo

--- Model_API/app.py
+++ Model_API/app.py
@@ -1,10 +1,19 @@
-from flask import Flask, render_templates, request
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+@app.route('/predict', methods=['POST'])
+def predict():
+     json_ = request.json
+     query_df = pd.DataFrame(json_)
+     query = pd.get_dummies(query_df)
+    
+     model = joblib.load('model.pkl')
+     prediction = model.predict(query)
+     return jsonify({'prediction': list(prediction)})
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run(debug=True)
