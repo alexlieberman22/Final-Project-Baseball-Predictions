d0bace20bf93c8a5f8da05f6f094e69de710d45b
alexlieberman22 <32115248+alexlieberman22@users.noreply.github.com>
2022-05-25 21:08:36 -0600

    Edits to app and html/ added source data info

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, request, render_template, jsonify
 import pandas as pd
 import joblib
@@ -9,35 +8,54 @@ app = Flask(__name__)
 def index():
      return render_template("index.html")
 
-@app.route('/predict1', methods=['GET','POST'])
-def predict():
-     # json = request.json
-     # querydf = pd.DataFrame(json)
+# @app.route('/predict_1', methods=['GET','POST'])
+# def predict():
+#      # json = request.json
+#      # querydf = pd.DataFrame(json)
 
-     # model = joblib.load('model.pkl')
-     # prediction = model.predict(query)
-     # return jsonify({'prediction': list(prediction)})
+#      # model = joblib.load('model.pkl')
+#      # prediction = model.predict(query)
+#      # return jsonify({'prediction': list(prediction)})
 
-     if request.method == 'POST':
-          my_json = request.json
-          number1 = my_json.get("number1")
-          number2 = my_json.get("number2")
-          prediction = int(number1) + int(number2)
-          return {"prediction": prediction}
+#      if request.method == 'POST':
+#           my_json = request.json
+#           number1 = my_json.get("number1")
+#           number2 = my_json.get("number2")
+#           prediction = int(number1) + int(number2)
+#           return {"prediction": prediction}
 
-     return render_template('predict_1.html')
+#      return render_template('predict_1.html')
 
 @app.route('/prediction_teams', methods=['GET','POST'])
 def prediction_1():
 
-     json = request.json
-     querydf = pd.DataFrame(json)
+     # json = request.json
+     # querydf = pd.DataFrame(json)
 
-     model = joblib.load('Models/Games_OLS.sav')
-     prediction = model.predict(querydf)
-     return jsonify({'prediction_wins': prediction})
+     # model = joblib.load('Models/Games_OLS.sav')
+     # prediction = model.predict(querydf)
+     # return jsonify({'prediction_wins': prediction})
+
+     if request.method == 'POST':
+          my_dict = request.form.to_dict()
+          # two_pointer = my_dict.get("2pointer")
+          # three_pointer = my_dict.get("3pointer")
+          # freethrow = my_dict.get("freethrow")
+          # assists = my_dict.get("assists")
+          # rebounds = my_dict.get("rebounds")
+          # attendance = my_dict.get("attendance")
 
 
+          querydf = pd.DataFrame(my_dict)
+          # print(querydf)
+          # model = joblib.load('Models/Games_OLS.sav')
+          # prediction = model.predict(querydf)
+          # print(prediction)
+          
+          return querydf
+          return render_template('prediction_teams.html', prediction_wins=prediction)
+          # prediction = int(number1) + int(number2)
+          # return render_template('predict_1.html', prediction=prediction)
 
 
      return render_template('prediction_teams.html')
